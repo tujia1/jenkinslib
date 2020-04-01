504711733eda4a5c3eb13d22020d3e580e40da3a
tujia1 <1207960652@qq.com>
2020-04-01 17:01:49 +0800

    Create build.groovy
    构建类型

---
+++ src/org/devops/build.groovy
@@ -0,0 +1,22 @@
+package org.devops
+package org.devops.tools
+
+def = new org.devops.tools()  
+//构建类型
+def Build(buildType,buildShell){
+    def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
+    
+    
+    tools.PrintMes("当前选择的构建类型为 ${buildType}","red")
+    buildHome= tool buildTools[buildType]
+    
+    if ("${buildType}" == "npm"){
+        
+        sh  """ 
+            export NODE_HOME=${buildHome} 
+            export PATH=\$NODE_HOME/bin:\$PATH 
+            ${buildHome}/bin/${buildType} ${buildShell}"""
+    } else {
+        sh "${buildHome}/bin/${buildType}  ${buildShell}"
+    }
+}
