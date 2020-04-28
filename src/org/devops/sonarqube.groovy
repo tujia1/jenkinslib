45f801d3feb9c66920124632070f2e83fb6f7695
tujia1 <1207960652@qq.com>
2020-04-28 18:05:32 +0800

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -4,27 +4,24 @@ package org.devops
 
 //扫描
 def SonarScan(projectName,projectDesc,projectPath){
-    def  Sonarserver = ""
-    def  SonarHome="/usr/local/sonar-scanner"
-    def  SonarDate = sh  returnStdout: true, script: 'date +%Y%m%d-%H%M'
-    SonarDate = SonarDate - "\n"
+    withSonarQubeEnv('My SonarQube Server') {
+        def  SonarHome = "/usr/local/sonar-scanner"
+        def  SonarDate = sh  returnStdout: true, script: 'date +%Y%m%d-%H%M'
+        SonarDate = SonarDate - "\n"
 
-    sh  """
-        ${SonarHome}/bin/sonar-scanner  -Dsonar.host.url=${Sonarserver}  \
-        -Dsonar.projectKey=${projectName}  \
-        -Dsonar.projectName=${projectName}  \
-        -Dsonar.projectVersion=${SonarDate} \
-        -Dsonar.login=admin \
-        -Dsonar.password=tj212331 \
-        -Dsonar.ws.timeout=30 \
-        -Dsonar.projectDescription=${projectDesc}  \
-        -Dsonar.links.homepage=http://www.baidu.com \
-        -Dsonar.sources=${projectPath} \
-        -Dsonar.sourceEncoding=UTF-8 \
-        -Dsonar.java.binaries=target/classes \
-        -Dsonar.java.test.binaries=target/test-classes \
-        -Dsonar.java.surefire.report=target/surefire-reports
+        sh  """
+            ${SonarHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName}  \
+            -Dsonar.projectName=${projectName}  \
+            -Dsonar.projectVersion=${SonarDate} \
+            -Dsonar.ws.timeout=30 \
+            -Dsonar.projectDescription=${projectDesc}  \
+            -Dsonar.links.homepage=http://www.baidu.com \
+            -Dsonar.sources=${projectPath} \
+            -Dsonar.sourceEncoding=UTF-8 \
+            -Dsonar.java.binaries=target/classes \
+            -Dsonar.java.test.binaries=target/test-classes \
+            -Dsonar.java.surefire.report=target/surefire-reports
         
-    """
-
+        """
+     }   
 }
